e04a9993c60618088b10299200b789587fe9d2ec
dankim <dkGithup2022@gmail.com>
2024-03-27 08:19:13 +0900

    organize  module directory -> make directory for core and common

--- app/app-web-api/src/main/java/com/devwiki/project/app/web/api/config/domainConfig.java
+++ app/app-web-api/src/main/java/com/devwiki/project/app/web/api/config/domainConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.devwiki.project.core.domain_service"})
+@ComponentScan(basePackages = {"com.devwiki.project.core.domain_service", "com.devwiki.project.core.domain"})
 public class domainConfig {
 }
